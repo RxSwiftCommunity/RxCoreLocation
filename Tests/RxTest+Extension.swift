0ed4968b893ab0ab621ec201de962a7bf5bb2c1c
Obi Bob Godwin <bobgodwinx@gmail.com>
2017-11-09 16:53:38 +0100

    Initial extension of `TestScheduler`
    Introduced `func record<O: ObservableConvertibleType>(source: O) -> TestableObserver<O.E>` into `TestScheduler`

---
+++ Tests/RxTest+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  RxTest+Extension.swift
+//  RxCoreLocation
+//
+//  Created by Bob Obi on 09.11.17.
+//  Copyright © 2017 RxCoreLocation. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import RxSwift
+import RxTest
+
+extension TestScheduler {
+     /// Builds testable observer for s specific observable sequence, binds it's results and sets up disposal.
+     /// parameter source: Observable sequence to observe.
+     /// returns: Observer that records all events for observable sequence.
+    func record<O: ObservableConvertibleType>(source: O) -> TestableObserver<O.E> {
+        let observer = self.createObserver(O.E.self)
+        let disposable = source.asObservable().bind(to: observer)
+        self.scheduleAt(100000) {
+            disposable.dispose()
+        }
+        return observer
+    }
+}
