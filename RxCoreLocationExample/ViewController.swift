7a8acaf33a82d3803a1fa98c1754c2ad8928fc8f
Obi Bob Godwin <bobgodwinx@gmail.com>
2017-11-13 14:28:15 +0100

    Initial examples tested on simulator and device

--- RxCoreLocationExample/ViewController.swift
+++ RxCoreLocationExample/ViewController.swift
@@ -17,15 +17,79 @@ class ViewController: UIViewController {
     
     let bag = DisposeBag()
     let manager = CLLocationManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        /// Setup CLLocationManager
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
-        manager.rx.activityType.debug("activityType").subscribe(onNext: {_ in}).disposed(by: bag)
-        manager.rx.isEnabled.debug("isEnabled").subscribe(onNext: { _ in }).disposed(by: bag)
-        manager.rx.didUpdateLocations.debug("didUpdateLocations").subscribe(onNext: { _ in }).disposed(by: bag)
-        manager.rx.location.debug("location").subscribe(onNext: { _ in }).disposed(by: bag)
+        
+        /// Start Subscribing
+        /// Works on simulator and device
+        /// Subscribe to didUpdateLocations
+        manager.rx
+            .didUpdateLocations
+            .debug("didUpdateLocations")
+            .subscribe(onNext: { _ in })
+            .disposed(by: bag)
+        
+        /// Subscribe to didChangeAuthorization
+        manager.rx
+            .didChangeAuthorization
+            .debug("didChangeAuthorization")
+            .subscribe(onNext: { _ in })
+            .disposed(by: bag)
+        
+        /// Subscribe to location
+        manager.rx
+            .location
+            .debug("location")
+            .subscribe(onNext: { _ in })
+            .disposed(by: bag)
+        
+        /// Subscribe to activityType
+        manager.rx
+            .activityType
+            .debug("activityType")
+            .subscribe(onNext: {_ in})
+            .disposed(by: bag)
+        
+        /// Subscribe to isEnabled
+        manager.rx
+            .isEnabled
+            .debug("isEnabled")
+            .subscribe(onNext: { _ in })
+            .disposed(by: bag)
+        
+        /// Subscribe to didError
+        manager.rx
+            .didError
+            .debug("didError")
+            .subscribe(onNext: { _ in })
+            .disposed(by: bag)
+        
+        ///Note works on Device
+        
+        /// Subscribe to didDetermineState
+        manager.rx
+            .didDetermineState
+            .debug("didDetermineState")
+            .subscribe(onNext: { _ in })
+            .disposed(by: bag)
+        
+        /// Subscribe to didReceiveRegion
+        manager.rx
+            .didReceiveRegion
+            .debug("didReceiveRegion")
+            .subscribe(onNext: { _ in })
+            .disposed(by: bag)
+        
+        /// Subscribe to didResume
+        manager.rx
+            .didResume
+            .debug("didResume")
+            .subscribe(onNext: { _ in })
+            .disposed(by: bag)
     }
 }
 
