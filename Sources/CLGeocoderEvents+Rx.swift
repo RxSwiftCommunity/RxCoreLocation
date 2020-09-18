158f6e06bd6bc44f341b5c0b40d8c377f757b94b
Zsolt Kovacs <lordzsoltt@gmail.com>
2020-09-18 17:47:49 +0200

    Add option to geocode address strings

--- Sources/CLGeocoderEvents+Rx.swift
+++ Sources/CLGeocoderEvents+Rx.swift
@@ -41,4 +41,62 @@ extension Reactive where Base: CLGeocoder {
             }
         }.unwrap()
     }
+
+    /// Reactive wrapper for `CLGeocoder`.`geocodeAddressString(_:completionHandler:)`
+    /// used to search for placemark
+    public func placemarks(with addressString: String) -> Observable<[CLPlacemark]> {
+        return Observable.create { [base] observer in
+            base.geocodeAddressString(addressString) { placemarks, _ in
+                guard let placemarks = placemarks else {
+                    observer.onError(NSError(domain: CLError.errorDomain, code: CLError.geocodeFoundNoResult.rawValue, userInfo: nil))
+                    return
+                }
+
+                observer.onNext(placemarks)
+                observer.onCompleted()
+            }
+            return Disposables.create {
+                base.cancelGeocode()
+            }
+        }
+    }
+
+    /// Reactive wrapper for `CLGeocoder`.`geocodeAddressString(_:in:completionHandler:)`
+    /// used to search for placemark
+    public func placemarks(with addressString: String, in region: CLRegion?) -> Observable<[CLPlacemark]> {
+        return Observable.create { [base] observer in
+            base.geocodeAddressString(addressString, in: region) { placemarks, _ in
+                guard let placemarks = placemarks else {
+                    observer.onError(NSError(domain: CLError.errorDomain, code: CLError.geocodeFoundNoResult.rawValue, userInfo: nil))
+                    return
+                }
+
+                observer.onNext(placemarks)
+                observer.onCompleted()
+            }
+            return Disposables.create {
+                base.cancelGeocode()
+            }
+        }
+    }
+
+    /// Reactive wrapper for `CLGeocoder`.`geocodeAddressString(_:in:preferredLocale:completionHandler:)`
+    /// used to search for placemark
+    @available(iOS 11.0, OSX 10.13, watchOSApplicationExtension 4.0, tvOS 11.0, *)
+    public func placemarks(with addressString: String, in region: CLRegion?, preferredLocale locale: Locale?) -> Observable<[CLPlacemark]> {
+        return Observable.create { [base] observer in
+            base.geocodeAddressString(addressString, in: region, preferredLocale: locale) { placemarks, _ in
+                guard let placemarks = placemarks else {
+                    observer.onError(NSError(domain: CLError.errorDomain, code: CLError.geocodeFoundNoResult.rawValue, userInfo: nil))
+                    return
+                }
+
+                observer.onNext(placemarks)
+                observer.onCompleted()
+            }
+            return Disposables.create {
+                base.cancelGeocode()
+            }
+        }
+    }
 }
