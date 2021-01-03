ed5fa92331ab3480fb6481affc0990984bd8d2d6
Bob <bobgodwinx@gmail.com>
2021-01-03 20:15:09 +0100

    RxSwift 6.0.0 Update
    1. Updated Cartfile
    2. Updated Package.swift
    3. Updated RxCoreLocation.podspec
    4. Updated circle.yml

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "RxCoreLocation", targets: ["RxCoreLocation"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1.1")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0")
     ],
     targets: [
         .target(name: "RxCoreLocation", dependencies: ["RxSwift", "RxCocoa"], path: "Sources")
